c214148212fbf98634f8e4025d314bcf7a2f63ac
Šipaila Steponas <steponas.sipaila@mif.stud.vu.lt>
2021-08-01 18:27:44 +0300

    looking good

--- my_inting/fetch_data.py
+++ my_inting/fetch_data.py
@@ -2,18 +2,23 @@ import requests
 import json
 import sqlite3
 
-my_key = "RGAPI-042eebca-f0c9-45fe-8293-824e9c103ec9"
 
-match_list_by_acc = "https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/uSeXv-1ss9cJrzBC9ZxEdWvyWLanYqK0ZhnIamsmdLhlqCDdDO9_QEgSLzjXB1-sqx06OceNxCi6LA/ids?type=ranked&start=699&count=100"
+match_list_by_acc = "https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/uSeXv-1ss9cJrzBC9ZxEdWvyWLanYqK0ZhnIamsmdLhlqCDdDO9_QEgSLzjXB1-sqx06OceNxCi6LA/ids?type=ranked&start=0&count=100"
 
 header = {
-    "api_key": "RGAPI-042eebca-f0c9-45fe-8293-824e9c103ec9"
+    "api_key": "RGAPI-7e0eb530-03a7-448f-85a3-65f59f8556e3"
 }
 
 # dict_keys(['id', 'accountId', 'puuid', 'name', 'profileIconId', 'revisionDate', 'summonerLevel'])
 # acc_info = json.loads(requests.get(summoner_by_name_url + "Catuin", header).content)
 # print(acc_info["puuid"])
-match_list = json.loads(requests.get(match_list_by_acc, header).content)
+bad_responses = [400, 401, 403, 404, 405, 415, 429, 500, 502, 503, 504]
+match_list_response = requests.get(match_list_by_acc, header)
+if match_list_response.status_code not in bad_responses:
+    match_list = json.loads(match_list_response.content)
+else:
+    print(match_list_response.content, "when trying to fetch match list_of_matches")
+    exit(1)
 
 conn = sqlite3.connect("my_inting_data.db")
 c = conn.cursor()
@@ -23,7 +28,7 @@ c = conn.cursor()
 list_of_matches = [match[0] for match in c.execute("SELECT game_id FROM match_history;").fetchall()]
 match_info_by_id = "https://europe.api.riotgames.com/lol/match/v5/matches/"
 
-bad_responses = [400, 401, 403, 404, 405, 415, 429, 500, 502, 503, 504]
+
 for match in match_list:
     if match not in list_of_matches:
         answer = requests.get(match_info_by_id + match, header)
--- my_inting/make_stats.py
+++ my_inting/make_stats.py
@@ -3,6 +3,7 @@ import json
 conn = sqlite3.connect("my_inting_data.db")
 
 
+# this method defines match status if I won or Lost
 def win_status(participants):
     puuid = "uSeXv-1ss9cJrzBC9ZxEdWvyWLanYqK0ZhnIamsmdLhlqCDdDO9_QEgSLzjXB1-sqx06OceNxCi6LA"
     for participant in participants:
@@ -10,13 +11,49 @@ def win_status(participants):
             return participant["win"]
 
 
-
+# Get match history
 c = conn.cursor()
 list_of_matches = c.execute("SELECT * FROM match_history;").fetchall()
 
-c.execute("DELETE FROM match_history WHERE game_id LIKE 'status';")
+#c.execute("DELETE FROM match_history WHERE game_id LIKE 'EUN1_2894086929';")
 conn.commit()
+conn.close()
 count = 0
+
+
+
+# Here specify what champs are locked in champ select
+options = "Cassiopeia,Lulu,Rengar,Kaisa,Vladimir,Ziggs,Vayne,TahmKench,Viego,Zac,Jhin,Talon,Vladimir,Sylas,Irelia,Seraphine,Kaisa,LeeSin,Katarina,Irelia,Zyra,XinZhao,Varus,Darius,Kassadin,Akshan,LeeSin,Ziggs,Soraka,Irelia,Darius,Kindred,Lucian,Kayn,Kaisa,Camille,Lulu,Kaisa,Orianna,MasterYi,XinZhao,LeeSin,Malphite,Sivir,Kayle,Khazix,Leblanc,Nasus,Lillia,Diana,Draven,Yuumi,Wukong,Akali,Diana,Ezreal,Graves,Jhin,TahmKench,Thresh,Garen,XinZhao,Leblanc,Samira,Janna,Vayne,Yasuo,Neeko,Kaisa,Nocturne,Camille,Qiyana,Mordekaiser,Samira,Nasus,Khazix,Seraphine,Nami"
+draft = {
+    "my_team": "Akali".split(","),
+    "enemy_team": "Singed".split(",")
+}
+
+
+
+def select_tip(champs_record):
+    #list_of_champ_record = [(champ, sum(champ_record[champ].values()), round(champ_record[champ][True]/sum(champ_record[champ].values()), 2)) for champ in champ_record.keys()]
+    dict_of_champ_record = {}
+
+    # {"wr": {True: 0, False: 0}, "syn_record": {}, "rec": []}
+    for champ, stats in champs_record.items():
+        for syn_champ, syn_stats in stats["syn_record"].items():
+            if syn_champ not in dict_of_champ_record.keys():
+                dict_of_champ_record[syn_champ] = {"win_rates_per_champ": [], "relevance": 0}
+            # from evry teammate champion we gather champs we played with win ratios, and add them to a list of 4 winratios
+            dict_of_champ_record[syn_champ]["win_rates_per_champ"].append(round(syn_stats[True]/sum(syn_stats.values()), 2))
+            dict_of_champ_record[syn_champ]["relevance"] += len(syn_stats.values())
+
+    tips = {"Bad": [], "Good": []}
+    for champ, ratios in dict_of_champ_record.items():
+        avrg = round(sum(ratios["win_rates_per_champ"])/len(ratios["win_rates_per_champ"]), 2)
+
+        if avrg >= 0.8:
+            tips["Good"].append([champ, ratios["relevance"], avrg])
+        if avrg < 0.5:
+            tips["Bad"].append([champ, ratios["relevance"], avrg])
+    return(tips)
+
 
 champion_data = {
     "with": {},
@@ -27,57 +64,109 @@ que_type = {
     700: 0,
     440: 0
 }
-draft = {
-    "my_team": "".split(","),
-    "enemy_team": "".split(",")
-}
 for match in list_of_matches:
     match_data = json.loads(match[1])
+
+    # patch or season or game type check
     if match_data["info"]["gameVersion"][:2] != "11" or match_data["info"]["queueId"] != 420:
         continue
+    
+    #total games count
     count += 1
+
+    # counting how many games of each que type
     que_type[match_data["info"]["queueId"]] += 1
     participants = match_data["info"]["participants"]
     win = win_status(participants)
+    
+    
     puuid = "uSeXv-1ss9cJrzBC9ZxEdWvyWLanYqK0ZhnIamsmdLhlqCDdDO9_QEgSLzjXB1-sqx06OceNxCi6LA"
+    
+    # check my champ for this game
+    for participant in participants:
+        if participant["puuid"] == puuid:
+            my_champ = participant["championName"]
+
+    # make stats of this game
     for participant in participants:
         if participant["puuid"] != puuid:
             champion = participant["championName"]
 
             # if participant has same win status as me he is on my team
-#
+            # tracking win ratio with that champ on my team overal, 
+            # tracking win ration of champs I played with that champion 
+            # and overal record of that champ on my team
             if win == participant["win"]:
                 if champion in draft["my_team"]:
                     if champion not in champion_data["with"].keys():
-                        champion_data["with"][champion] = {"wr": {True: 0, False: 0}, "rec": []}
+                        champion_data["with"][champion] = {"wr": {True: 0, False: 0}, "syn_record": {}, "rec": []}
                     champion_data["with"][champion]["wr"][win] += 1
+
+                    if my_champ not in champion_data["with"][champion]["syn_record"].keys():
+                        champion_data["with"][champion]["syn_record"][my_champ] = {True: 0, False: 0}
+                    champion_data["with"][champion]["syn_record"][my_champ][win] += 1
+
                     champion_data["with"][champion]["rec"].append(int(win))
+
+            # tracking win ratio with that champ on enemy team overal, 
+            # tracking win ration of champs I played against that champion 
+            # and overal record of that champ on enemy team vs me
             else:
                 if champion in draft["enemy_team"]:
                     if champion not in champion_data["against"].keys():
-                        champion_data["against"][champion] = {"wr": {True: 0, False: 0}, "rec": []}
+                        champion_data["against"][champion] = {"wr": {True: 0, False: 0}, "syn_record": {}, "rec": []}
                     champion_data["against"][champion]["wr"][win] += 1
+
+                    if my_champ not in champion_data["against"][champion]["syn_record"].keys():
+                        champion_data["against"][champion]["syn_record"][my_champ] = {True: 0, False: 0}
+                    champion_data["against"][champion]["syn_record"][my_champ][win] += 1
+
                     champion_data["against"][champion]["rec"].append(int(win))
 
-conn.close()
+
 
 list_with = []
 list_against = []
 for champ, stats in champion_data["with"].items():
-    list_with.append((champ, sum(stats["wr"].values()), round(stats["wr"][True]/sum(stats["wr"].values()), 2), stats["rec"][-10:]))
+    list_with.append((
+        champ, 
+        sum(stats["wr"].values()), 
+        round(stats["wr"][True]/sum(stats["wr"].values()), 2), 
+        stats["rec"][-5:],
+        ))
 
 for champ, stats in champion_data["against"].items():
-    list_against.append((champ, sum(stats["wr"].values()), round(stats["wr"][True]/sum(stats["wr"].values()), 2), stats["rec"][-10:]))
+    list_against.append((champ, sum(stats["wr"].values()), round(stats["wr"][True]/sum(stats["wr"].values()), 2), stats["rec"][-5:]))
+
+print("\n#######################################")
+print("######### THIS IS THE REPORT ##########")
+print("#######################################")
 
 
+print("\nOUR_TEAM")
 for item in list_with:
     print(item)
-print(round(sum([wr[2] for wr in list_with])/len(list_with), 2))
+tips = select_tip(champion_data["with"])
+print("\nGood:\n", tips["Good"])
+print("\nBAD:\n",tips["Bad"])
+print("\nAverage chance: ", round(sum([wr[2] for wr in list_with])/len(list_with), 2))
+
+
+print("\n\n\n")  
+print("ENEMY_TEAM")
 for item in list_against:
     print(item)
-print(round(
-    sum([wr[2] for wr in list_against])/len(list_against), 2
-    ))
+
+tips = select_tip(champion_data["against"])
+print("\nGood:\n", tips["Good"])
+print("\nBAD:\n",tips["Bad"])
+print("\nAverage chance: ", round(sum([wr[2] for wr in list_against])/len(list_against), 2))
+
+
+print("\n\n\n")
+print("Total games: ", count)
+print("Games per queue type registered: ", que_type)
+
 
 
 # list_with = sorted(list_with, key=lambda x: x[2])
@@ -89,6 +178,3 @@ print(round(
 # list_against = sorted(list_against, key=lambda x: x[2])
 # for item in list_against:
 #     print(item)
-
-print(count)
-print(que_type)
