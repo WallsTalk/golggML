6393086a62abce1c67325c62242dc7c59712ed4e
Šipaila Steponas <steponas.sipaila@mif.stud.vu.lt>
2021-09-19 13:29:14 +0300

    ww

--- pro_leagues/build_db/randoms/test.py
+++ pro_leagues/build_db/randoms/test.py
@@ -9,10 +9,35 @@ headers = {
     "Accept": "application/json",
     "Authorization": "Bearer cXP58-fafBjtvjkPufepwqtGY4N6xPNZL9bOwP8cwP7LDFo23RE"
 }
-url = "https://api.pandascore.co/lol/leagues"
 
-query_string = {"filter[id]": "4197", "filter[series][id]": "3669"}
-LEC = requests.request("GET", url, headers=headers, params=query_string).text[1:-1]
-print(LEC)
-#for item in leagues:
-   # print(item)
+
+#GET ALL LEAGUES for dataset
+# url = "https://api.pandascore.co/lol/leagues"
+# leagues_info = {}
+# for league_to_check in ["LCK", "LPL", "LCS", "LEC"]:
+#    querystring = {"filter[name]": league_to_check}
+#    response = requests.request("GET", url, headers=headers, params=querystring).text[2:-3]
+#    for league in response.split("}},{"):
+#       league_str = "{" + league + "}}"
+#       leagues_info[league_to_check] = league_str
+
+# with open("leagues.txt", "w") as leagues:
+#    leagues.write(json.dumps(leagues_info))
+
+
+
+
+
+
+url = "https://api.pandascore.co/lol/series/%s/teams"
+
+with open("leagues.txt", "r") as leagues:
+   leagues_json = json.loads(leagues.read())
+   for league, info in leagues_json.items():
+      info_json = json.loads(info)
+      for serie in info_json["series"]:
+         if "Summer 2021" in serie.values():
+            response = requests.get(url % serie["id"], headers=headers).text
+            print(response + "\n")
+
+            
