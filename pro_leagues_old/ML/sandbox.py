a537bd934d14007c33e44e45a0235228bf118d43
Šipaila Steponas <steponas.sipaila@mif.stud.vu.lt>
2021-10-02 15:19:49 +0300

    pandaaaas

--- pro_leagues_old/ML/sandbox.py
+++ pro_leagues_old/ML/sandbox.py
@@ -17,7 +17,7 @@ c = conn.cursor()
 games = pd.read_sql_query("SELECT * FROM game;", conn)
 gt_picks = pd.read_sql_query("SELECT * FROM game_team_picks;", conn)
 gt_stats = pd.read_sql_query("SELECT * FROM game_team_stats;", conn)
-teams = dict(map(lambda team: (team[0], team[1]), c.execute("SELECT * FROM team;").fetchall()))
+teams = dict(map(lambda team: (team[1], team[0]), c.execute("SELECT * FROM team;").fetchall()))
 
 conn.commit()
 conn.close()
@@ -25,7 +25,7 @@ conn.close()
 df = pd.merge(games, gt_stats, on="game_id")
 df = pd.merge(df, gt_picks, on=["game_id", "team_id"])
 df['game_date'] = pd.to_datetime(df['game_date'])
-df[df['game_date'] > datetime.datetime(2021, 5, 1)]
+df = df[df['game_date'] > datetime.datetime(2021, 5, 1)]
 
 
 # df = df.set_index(['game_id'])
@@ -40,7 +40,7 @@ df[df['game_date'] > datetime.datetime(2021, 5, 1)]
 stat_types = df["stat_type"].unique().tolist()
 roles = ["top", "jg", "mid", "adc", "sup"]
 team_stats = {}
-for team_id in teams.keys():
+for team_id in teams.values():
     team_stats[team_id] = {}
     team_df = df[df["team_id"] == team_id]
     for role in roles:
@@ -54,14 +54,14 @@ for team_id in teams.keys():
                 column = column.apply(lambda x: 0 if x == 'Perfect KDA' else float(x))
                 column = column.apply(lambda x: column.max() if x == 'Perfect KDA' else x)
             
-            team_stats[team_id][role][stat] = pd.to_numeric(column).mean().round(0)
+            team_stats[team_id][role][stat] = "{0:.2f}".format(pd.to_numeric(column).mean())
         #opt2
         
-        team_stats[team_id][role] = dict([(stat, team_df[role][team_df["stat_type"]==stat].mean()) for stat in stat_types])
+#        team_stats[team_id][role] = dict([(stat, team_df[role][team_df["stat_type"]==stat].mean()) for stat in stat_types])
         #opt1
         #team_stats[team_id][role] = dict([(stat, team_df[role][team_df["stat_type"]==stat].tolist()) for stat in stat_types])
   
-
+#match_up = ['Fnatic', '']
 # >>> team_stats.keys()
 # dict_keys([1105, 1109, 1107, 1116, 1113, 1111, 1104, 1102, 1103, 1108, 1112, 1114, 1110, 1101, 1106, 1117, 1115, 1154, 1151, 1148, 1150, 1153, 1147, 1149, 1152, 1156, 1155, 1092, 1093, 1096, 1098, 1097, 1091, 1125, 1094, 1126, 1099, 1129, 1133, 1128, 1132, 1131, 1130, 1134, 1136, 1127, 1135])
 # >>> print(team_stats[1105].keys())
@@ -74,6 +74,27 @@ for team_id in teams.keys():
 # 73
 # >>> print(teams[1105])
 # Invictus Gaming
+
+# Leaderboard for KDA
+worlds_teams = ['Royal Never Give Up', 'Funplus Phoenix', 'Edward Gaming', 'Rogue', 'Fnatic', 'MAD Lions', 'DWG KIA', 'T1', 'Gen.G eSports', '100 Thieves', 'Team Liquid']
+
+
+def show_comparison_stats(stat_to_check):
+    data_kda = {"team":[], "top":[], "jg":[], "mid":[], "adc":[], "sup":[]}
+    for team in worlds_teams:
+        data_kda["team"].append(team)
+        for role in team_stats[teams[team]]:
+            data_kda[role].append(float(team_stats[teams[team]][role][stat_to_check]))
+    kda = pd.DataFrame(data=data_kda)
+    print(stat_to_check)
+    for role in roles:
+        print(role)
+        print(kda.sort_values(role, ascending=False))
+# lambda x: pd.to_numeric()
+
+show_comparison_stats('VisionScore')
+
+        
 
 
 
