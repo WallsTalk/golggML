abec72d0e4de5bed1d942a0cf5cd7956a75196cd
Šipaila Steponas <steponas.sipaila@mif.stud.vu.lt>
2021-10-03 13:14:58 +0300

    pandaaaas

--- pro_leagues_old/ML/sandbox.py
+++ pro_leagues_old/ML/sandbox.py
@@ -13,11 +13,14 @@ conn = sqlite3.connect("stats.db")
 c = conn.cursor()
 
 # STEP 1 MAKE a dataframe of team/picks/stats tables for wanted date
-
+# region = {"LEC": [], "LPL": [], "LCK": [], "LCS": []}
+# for t in c.execute("SELECT * FROM team;").fetchall():
+#     region[t[2]].append([t[1], t[0]])
 games = pd.read_sql_query("SELECT * FROM game;", conn)
 gt_picks = pd.read_sql_query("SELECT * FROM game_team_picks;", conn)
 gt_stats = pd.read_sql_query("SELECT * FROM game_team_stats;", conn)
 teams = dict(map(lambda team: (team[1], team[0]), c.execute("SELECT * FROM team;").fetchall()))
+
 
 conn.commit()
 conn.close()
@@ -54,13 +57,8 @@ for team_id in teams.values():
                 column = column.apply(lambda x: 0 if x == 'Perfect KDA' else float(x))
                 column = column.apply(lambda x: column.max() if x == 'Perfect KDA' else x)
             
-            team_stats[team_id][role][stat] = "{0:.2f}".format(pd.to_numeric(column).mean())
-        #opt2
-        
-#        team_stats[team_id][role] = dict([(stat, team_df[role][team_df["stat_type"]==stat].mean()) for stat in stat_types])
-        #opt1
-        #team_stats[team_id][role] = dict([(stat, team_df[role][team_df["stat_type"]==stat].tolist()) for stat in stat_types])
-  
+            team_stats[team_id][role][stat] = float("{0:.2f}".format(pd.to_numeric(column).mean()))
+
 #match_up = ['Fnatic', '']
 # >>> team_stats.keys()
 # dict_keys([1105, 1109, 1107, 1116, 1113, 1111, 1104, 1102, 1103, 1108, 1112, 1114, 1110, 1101, 1106, 1117, 1115, 1154, 1151, 1148, 1150, 1153, 1147, 1149, 1152, 1156, 1155, 1092, 1093, 1096, 1098, 1097, 1091, 1125, 1094, 1126, 1099, 1129, 1133, 1128, 1132, 1131, 1130, 1134, 1136, 1127, 1135])
@@ -84,7 +82,7 @@ def show_comparison_stats(stat_to_check):
     for team in worlds_teams:
         data_kda["team"].append(team)
         for role in team_stats[teams[team]]:
-            data_kda[role].append(float(team_stats[teams[team]][role][stat_to_check]))
+            data_kda[role].append(team_stats[teams[team]][role][stat_to_check])
     kda = pd.DataFrame(data=data_kda)
     print(stat_to_check)
     for role in roles:
@@ -94,9 +92,33 @@ def show_comparison_stats(stat_to_check):
 
 show_comparison_stats('VisionScore')
 
-        
+
+# region kda insights
+for reg in region.keys():
+    for role in roles:
+        reg_teams = list(map(lambda x: float(team_stats[x[1]][role]['KDA']), region[reg]))
+        print(reg, role , min(reg_teams), max(reg_teams),  max(reg_teams) - min(reg_teams))
+
+### for avg analysis use: 'GPM', 'DPM', 'TotaldamagetoChampion', 'Totaldamagetaken'
+### for defeat analysis use: 'DMG%', 'GOLD%', 'KP%'
+### for tempo analysis use: 'GD@15', 'CSD@15', 'XPD@15', 'LVLD@15', 'K+APerMinute'
 
 
+
+
+# this function looks at avg dmg dealt against other teams avg dmg taken "{0:.2f}".format(
+def calculate_dmg_trade(team1, team2):
+    t1_score = {"dealt": 0, "taken": 0}
+    t2_score = {"dealt": 0, "taken": 0}
+    for role in roles:
+        t1_score["dealt"] += team_stats[team1][role]['TotaldamagetoChampion']
+        t1_score["taken"] += team_stats[team1][role]['Totaldamagetaken']
+        t2_score["dealt"] += team_stats[team2][role]['TotaldamagetoChampion']
+        t2_score["taken"] += team_stats[team2][role]['Totaldamagetaken']
+    print(t1_score["taken"] - t2_score["dealt"])
+    print(t2_score["taken"] - t1_score["dealt"])
+
+calculate_dmg_trade(teams['Rogue'], teams['DWG KIA'])
 
 
 
