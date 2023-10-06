7139ceaa4fb34ec40286dc5ee6dfbd8e1365ec0e
WallsTalk <WallsTalk>
2023-10-06 20:37:15 +0300

    2023

--- 2023/check_out_the_data_dude.py
+++ 2023/check_out_the_data_dude.py
@@ -3,28 +3,22 @@
 
 import json
 import pandas as pd
-
+import os
 
 def main():
-    with open("game_collection_12", "r") as game_history:
+    season = "13"
+    current_game_colection = "game_collection2_" + season
+    with open(os.path.join(os.getcwd(), "history", current_game_colection), "r") as game_history:
         game_history = [json.loads(game) for game in game_history.read().split("\n")[:-1]]
-    count = {}
-    count2 = 0
-    tur = []
-    for game in game_history:
-        if not game["events"]:
-            if game["turney"] not in count:
-                count[game["turney"]] = 0
-            count[game["turney"]] += 1
 
-    print(count, count2)
-    print(set(tur))
-    # game_stats = [game["stats"] for game in game_history]
-    # #df = pd.DataFrame([game["stats"] for game in game_history])
-    # df = pd.DataFrame([{stat.replace(" ", "-").replace("@", "at").replace("%", "-proc").replace("'", "").replace("+", "").lower() + ("B" if i < 5 else "R") + game["stats"]["Role"][i][0]: vals[i] for stat, vals in game["stats"].items() for
-    #   i in range(10)} for game in game_history])
-    # #df[df.columns.map(lambda x: "B_" + x)] = df.applymap(lambda x: x[:5])
-    # #df = pd.DataFrame({("B_" + key: vals[:5], "R_" + key: vals[5:]) for game in game_stats for key, vals in game.items()})
+    game_stats = [game["stats"] for game in game_history]
+    #df = pd.DataFrame([game["stats"] for game in game_history])
+    df = pd.DataFrame([{"teamB": game["blue_team"], "resB": game["blue_result"], "teamR": game["red_team"], "resR": game["blue_result"]} | {stat.replace(" ", "-").replace("@", "at").replace("%", "-proc").replace("'", "").replace("+", "").lower() + ("B" if i < 5 else "R") + game["stats"]["Role"][i][0]: vals[i] for stat, vals in game["stats"].items() for
+      i in range(10)} for game in game_history])
+    x=1
+    print(df)
+    #df[df.columns.map(lambda x: "B_" + x)] = df.applymap(lambda x: x[:5])
+    #df = pd.DataFrame({("B_" + key: vals[:5], "R_" + key: vals[5:]) for game in game_stats for key, vals in game.items()})
     #
     # x = 1
 
