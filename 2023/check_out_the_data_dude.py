623bce6780e298503fba4f1acb9fe1f0e213122e
WallsTalk <WallsTalk>
2023-10-07 21:36:14 +0300

    2023

--- 2023/check_out_the_data_dude.py
+++ 2023/check_out_the_data_dude.py
@@ -11,12 +11,48 @@ def main():
     with open(os.path.join(os.getcwd(), "history", current_game_colection), "r") as game_history:
         game_history = [json.loads(game) for game in game_history.read().split("\n")[:-1]]
 
-    game_stats = [game["stats"] for game in game_history]
-    #df = pd.DataFrame([game["stats"] for game in game_history])
-    df = pd.DataFrame([{"teamB": game["blue_team"], "resB": game["blue_result"], "teamR": game["red_team"], "resR": game["red_result"]} | {stat.replace(" ", "-").replace("@", "at").replace("%", "-proc").replace("'", "").replace("+", "").lower() + ("B" if i < 5 else "R") + game["stats"]["Role"][i][0]: vals[i] for stat, vals in game["stats"].items() for
-      i in range(10)} for game in game_history])
+    statsdf = []
+    for game in game_history:
+        a = {
+            "teamB": game["blue_team"],
+            "resB": game["blue_result"],
+            "teamR": game["red_team"],
+            "resR": game["red_result"],
+
+        }
+        champ2role = {game["stats"]["champs"][i].replace(" ", "").lower(): game["stats"]["Role"][i] for i in range(10)}
+        for stat, vals in game["stats"].items():
+            stat_name = stat.replace(" ", "-").replace("@", "at").replace("%", "-proc").replace("'", "").replace(
+                "+", "").lower()
+            for i in range(10):
+                if i < 5:
+                    a[stat_name + "B" + game["stats"]["Role"][i][0]] = vals[i]
+                else:
+                    a[stat_name + "R" + game["stats"]["Role"][i][0]] = vals[i]
+
+        for i in range(len(game["events"])):
+            event = game["events"][i]
+            a[f"time{i}"] = event[0]
+            if event[5] == "kill;gold":
+                target = champ2role[event[5].lower()][0]
+            else:
+                target = event[6]
+
+
+            a[f"event{i}"] = event[1][0]+ ";".join([champ2role[champ.lower()][0] for champ in event[3].split(";") if champ]) + event[4] + target
+
+            x=1
+        statsdf.append(a)
+    statsdf = pd.DataFrame(statsdf)
+
+
+    #     b = { stat.replace(" ", "-").replace("@", "at").replace("%", "-proc").replace("'", "").replace("+", "").lower() + ("B" if i < 5 else "R") + game["stats"]["Role"][i][0]: vals[i] for stat, vals in game["stats"].items()}
+    # statsdf = pd.DataFrame([{"teamB": game["blue_team"], "resB": game["blue_result"], "teamR": game["red_team"], "resR": game["red_result"]} | {stat.replace(" ", "-").replace("@", "at").replace("%", "-proc").replace("'", "").replace("+", "").lower() + ("B" if i < 5 else "R") + game["stats"]["Role"][i][0]: vals[i] for stat, vals in game["stats"].items() for
+    #   i in range(10)} for game in game_history])
+    # champ2player = {}
+    # eventdf = pd.DataFrame([[{f"time{i}": game["events"][i][0], f"event{i}": [game["events"][i][1], game["events"][i][2], game["events"][i][4], game["events"][i][6]] } for i in range(0, len(game["events"]))] for game in game_history])
     x=1
-    print(df)
+
     #df[df.columns.map(lambda x: "B_" + x)] = df.applymap(lambda x: x[:5])
     #df = pd.DataFrame({("B_" + key: vals[:5], "R_" + key: vals[5:]) for game in game_stats for key, vals in game.items()})
     #
