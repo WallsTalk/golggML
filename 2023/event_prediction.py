5e4c5c212937ba8d43415f8eca7c34407bd254d6
WallsTalk <WallsTalk>
2023-10-08 23:10:35 +0300

    now

--- 2023/event_prediction.py
+++ 2023/event_prediction.py
@@ -5,7 +5,7 @@ from sklearn.ensemble import RandomForestRegressor
 
 
 def main ():
-    df = pd.read_csv('decent_data.csv')
+    df = pd.read_csv('12.csv')
     df["turney"] = df["turney"].apply(lambda x: x.split(" 20")[0])
     all_turneys = list(set(df["turney"].tolist()))
     all_turneys = {all_turneys[i]: i for i in range(len(all_turneys))}
@@ -19,21 +19,22 @@ def main ():
     with open("event_dict.json", "r") as eventsf:
         events_dict = {v: k for k, v in json.load(eventsf).items()}
 
-    df = df.dropna(subset=["time0"])
+    df = df.dropna(subset=["time0", "event0"])
 
-    season_df = df.loc[:, ["turneyid", "teamidB", "teamidR"] + list(df.filter(regex="pid").columns)]
+    season_df = df.loc[:, list(df.filter(regex="pid").columns) + ["turneyid", "teamidB", "teamidR"]]
     blue_teams = ["Team BDS", "LOUD", "Detonation FocusMe"] #"Movistar R7", "LOUD", "DetonatioN FocusMe"
     red_teams = ["Golden Guardians", "GAM Esports", "CTBC Flying Oyster"] #"PSG Talon", "GAM Esports", "CTBC Flying Oyster"
-    predict_df = pd.DataFrame({
+    predict_df = pd.DataFrame(
+    {col: [df.loc[df["teamB"] == team, col].iloc[0] for team in blue_teams] for col in df[list(df.filter(regex="pidB"))].columns} |
+    {col: [df.loc[df["teamR"] == team, col].iloc[0] for team in red_teams] for col in df[list(df.filter(regex="pidR"))].columns} |
+    {
         "turneyid": [all_turneys["World Championship"]] * len(blue_teams),
         "teamidB": [all_teams[team] for team in blue_teams],
         "teamidR": [all_teams[team] for team in red_teams],
-    } |
-    {col: [df.loc[df["teamB"] == team, col].iloc[0] for team in blue_teams] for col in df[list(df.filter(regex="pidB"))].columns} |
-    {col: [df.loc[df["teamR"] == team, col].iloc[0] for team in red_teams] for col in df[list(df.filter(regex="pidR"))].columns}
+    }
     )
 
-    forest_model = RandomForestRegressor()
+    forest_model = RandomForestRegressor(n_estimators=3000, max_depth=50, warm_start=True)
     i=0
 
     finished_df = pd.DataFrame(df.columns)
@@ -60,8 +61,9 @@ def main ():
 
         print(predict_df.iloc[:, 13:])
         finished_index = predict_df[(predict_df[event_str] == 6166) | (predict_df[event_str] == 417)].index
-        finished_df.loc[len(finished_df)] = finished_index
-        predict_df.drop(finished_index, inplace=True)
+        if len(finished_index) > 0:
+            finished_df.loc[len(finished_df)] = finished_index
+            predict_df.drop(finished_index, inplace=True)
 
         if len(predict_df.index) > 0:
             i+=1
