a04860006f08656c4c0b0112e1a78939cb28cf7c
WallsTalk <WallsTalk>
2023-10-09 16:02:33 +0300

    now

---
+++ 2023/crystal_ball.py
@@ -0,0 +1,12 @@
+
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+
+
+def main ():
+    df = pd.read_csv('decent_data2.csv')
+
+
+
+if __name__== "__main__":
+    main()
--- 2023/event_prediction.py
+++ 2023/event_prediction.py
@@ -3,22 +3,11 @@ import json
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 
-
+# C:/Users/PC/PycharmProjects/golggML/2023/event_prediction.py:8: DtypeWarning: Columns (822,825,828,831,834,837,840,843,846,849,852,855,858,861,864,867,870,873,876,879,882,885,888,891,894,897,900,903,906,909,912,915,918,921,924,927,930,933,936,939,942,945,948,951,954,957,960,963,966,969,972,975,978,981,984,987,990,993,996,999,1002,1005,1008,1011,1014,1017,1020,1023,1026,1029,1032,1035,1038,1041,1044,1047,1050,1053,1056,1059,1062,1065,1068,1071,1074,1077,1080,1083,1086,1089,1092,1095,1098,1101,1104,1107,1110,1113,1116,1119,1122,1125,1128,1131,1134,1137,1140,1143,1146,1149,1152,1155,1158,1161,1164,1167,1170,1173,1176,1179,1182,1185,1188,1191,1194,1197,1200,1203,1206,1209,1212,1215,1218,1221,1224,1227,1230,1233,1236,1239) have mixed types. Specify dtype option on import or set low_memory=False.
 def main ():
-    df = pd.read_csv('12.csv')
-    df["turney"] = df["turney"].apply(lambda x: x.split(" 20")[0])
-    all_turneys = list(set(df["turney"].tolist()))
-    all_turneys = {all_turneys[i]: i for i in range(len(all_turneys))}
-    df["turneyid"] = df.replace({"turney": all_turneys})["turney"]
-    all_teams = list(set([item for col, vals in df[["teamB", "teamR"]].items() for item in vals.tolist()]))
-    all_teams = {all_teams[i]: i for i in range(len(all_teams))}
-    df["teamidB"] = df.replace({"teamB": all_teams})["teamB"]
-    df["teamidR"] = df.replace({"teamR": all_teams})["teamR"]
-
-    with open("event_dict.json", "r") as eventsf:
-        events_dict = {v: k for k, v in json.load(eventsf).items()}
-
-    df = df.dropna(subset=["time0", "event0"])
+    df = pd.read_csv('decent_data2.csv')
+    df.drop(df.filter(regex="event[0-9]+"), axis=1, inplace=True)
+    df.dropna(subset=["time0", "eventid0"], inplace=True)
 
     season_df = df.loc[:, list(df.filter(regex="pid").columns) + ["turneyid", "teamidB", "teamidR"]]
     blue_teams = ["Team BDS", "LOUD", "Detonation FocusMe"] #"Movistar R7", "LOUD", "DetonatioN FocusMe"
@@ -27,34 +16,32 @@ def main ():
     {col: [df.loc[df["teamB"] == team, col].iloc[0] for team in blue_teams] for col in df[list(df.filter(regex="pidB"))].columns} |
     {col: [df.loc[df["teamR"] == team, col].iloc[0] for team in red_teams] for col in df[list(df.filter(regex="pidR"))].columns} |
     {
-        "turneyid": [all_turneys["World Championship"]] * len(blue_teams),
-        "teamidB": [all_teams[team] for team in blue_teams],
-        "teamidR": [all_teams[team] for team in red_teams],
+        "turneyid": [df.loc[df["turney"] == "World Championship", "turneyid"].iloc[0]] * len(blue_teams),
+        "teamidB": [df.loc[df["teamB"] == team, "teamidB"].iloc[0] for team in blue_teams],
+        "teamidR": [df.loc[df["teamB"] == team, "teamidR"].iloc[0] for team in red_teams],
     }
     )
 
-    time_model = RandomForestRegressor(warm_start=True, random_state=5)
-    event_model = RandomForestRegressor(n_estimators=1000, warm_start=True, random_state=5)
+    time_model = RandomForestRegressor()
+    event_model = RandomForestRegressor()
     i=0
 
     finished_df = pd.DataFrame(df.columns)
     while True:
         time_str = "time" + str(i)
         time = df[time_str]
-
+        x=1
         time_model.fit(season_df, time)
         predict_df[time_str] = time_model.predict(predict_df)
         predict_df[time_str] = predict_df[time_str].apply(lambda x: round(x))
         season_df[time_str] = time
 
-
-
-        event_str = "event" + str(i)
+        event_str = "eventid" + str(i)
         event = df[event_str]
 
         event_model.fit(season_df, event)
         predict_df[event_str] = event_model.predict(predict_df)
-        predict_df[event_str] = predict_df[event_str].apply(lambda match: min(events_dict, key=lambda x: abs(x-match)))
+        predict_df[event_str] = predict_df[event_str].apply(lambda x: round(x))
         season_df[event_str] = event
 
 
